20e08681ee927ced8f4b5ffcad16a1da82c2c0a7
sambacarlson <sambacarlson@gmail.com>
2024-12-07 11:15:00 +0100

    added packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { TextEditor } from "./TextEditor";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p className="Title">POC lexical text editor</p>
+      <div className="">
+        <p className="Title">Text editor</p>
+        <div className="EditorContainer">
+          <TextEditor />
+        </div>
+      </div>
+      <div className="">
+        <p className="Title">Output raw</p>
+        <div className="output">sadfadf</div>
+      </div>
     </div>
   );
 }
